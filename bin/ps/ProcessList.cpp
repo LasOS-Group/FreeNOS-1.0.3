83abd337c5f941dfe8ed1667cf32b1b81ef25fdc
SerlyTeymoorian <98438828+SerlyTeymoorian@users.noreply.github.com>
2022-10-03 17:49:57 -0700

    adding priority level requirements

--- bin/ps/ProcessList.cpp
+++ bin/ps/ProcessList.cpp
@@ -34,9 +34,16 @@ ProcessList::Result ProcessList::exec()
 {
     const ProcessClient process;
     String out;
+    boolean priorityUsed = false; 
 
     // Print header
-    out << "ID  PARENT  USER GROUP STATUS     CMD\r\n";
+    if(arguments().getFlag().count == 0){
+        out << "ID  PARENT  USER GROUP STATUS     CMD\r\n";
+    }
+    else if(arguments().get("priorityList"){
+        out << "ID PRIORITY  PARENT  USER GROUP STATUS     CMD\r\n";
+        priorityUsed = true; 
+    }
 
     // Loop processes
     for (ProcessID pid = 0; pid < ProcessClient::MaximumProcesses; pid++)
@@ -46,15 +53,31 @@ ProcessList::Result ProcessList::exec()
         const ProcessClient::Result result = process.processInfo(pid, info);
         if (result == ProcessClient::Success)
         {
-            DEBUG("PID " << pid << " state = " << *info.textState);
+            //if the prioirty flag -l was entered then print the priority levels of all proccesses 
+            if(priorityUsed) {
+                
+                DEBUG("PID " << pid << " state = " << *info.textState);
 
-            // Output a line
-            char line[128];
-            snprintf(line, sizeof(line),
-                    "%3d %7d %4d %5d %10s %32s\r\n",
-                     pid, info.kernelState.parent,
-                     0, 0, *info.textState, *info.command);
-            out << line;
+                // Output a line
+                char line[128];
+                snprintf(line, sizeof(line),
+                        "%3d %7d %4d %5d %10s %32s\r\n",
+                         pid, info.kernelState.parent,
+                         0, 0, *info.textState, *info.command);
+                out << line;
+            }
+            //no priority level printed 
+            else {
+                DEBUG("PID " << pid << " state = " << *info.textState << " priority = " << info.kernelState.priority);
+
+                // Output a line
+                char line[128];
+                snprintf(line, sizeof(line),
+                        "%3d %7d %4d %5d %10s %32s\r\n",
+                         pid, info.kernelState.priority, info.kernelState.parent,
+                         0, 0, *info.textState, *info.command);
+                out << line;
+            }
         }
     }
 
